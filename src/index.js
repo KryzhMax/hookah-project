7b132f1283c6c6ee7f633352e8946d107e4b34fe
MaxKryzh <maximkryzhanovsky@gmail.com>
2023-02-27 17:52:09 +0200

    updating scroll

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// import './libs/gsap/gsap.min';
+// import './libs/gsap/ScrollSmoother.min';
+// import './libs/gsap/ScrollTrigger.min';
+
+import './js/app';
+import './js/menu';
+import './js/products';
+import './js/franchise';
+
+import AOS from 'aos';
+import './js/scroll';
+AOS.init();
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,18 @@
+let debounceTimeout;
+const home = document.querySelector('[data-home]');
+
 window.addEventListener('scroll', e => {
-  document.body.style.cssText += `--scrollTop: ${this.scrollY}px`;
+  debounceTimeout = setTimeout(() => {
+    // document.body.classList.toggle('scrolling', window.scrollY > 0);
+    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`;
+  }, 10);
+  clearTimeout(debounceTimeout);
+
+  if (window.scrollY > 200) {
+    home.style.display = 'block';
+  } else {
+    home.style.display = 'none';
+  }
 });
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
@@ -7,26 +20,74 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 let smoother = ScrollSmoother.create({
   wrapper: '#scroll-wrapper',
   content: '#smooth-content',
-  smooth: 1.5,
-  smoothTouch: 0.1,
+  smooth: 1,
+  smoothTouch: 0.3,
   effects: true,
 });
 
 const products = document.querySelectorAll('[data-products]');
-const info = document.querySelectorAll('[data-info]');
-const aboutUs = document.querySelectorAll('[data-about]');
+const advantages = document.querySelectorAll('[data-info]');
+const about = document.querySelectorAll('[data-about]');
 const contacts = document.querySelectorAll('[data-contacts]');
+const links = [products, advantages, about, contacts];
 
-function buttonListeners(elem, name) {
-  console.log(name);
-  for (let i = 0; i < elem.length; i++) {
-    elem[i].addEventListener('click', e => {
+function buttonListeners(name) {
+  links.forEach(link =>
+    link.addEventListener('click', e => {
       smoother.scrollTo(`#${name}`, true);
-    });
-  }
-  return elem;
+      buttonListeners(`#${link}`);
+    })
+  );
+  return links;
 }
-buttonListeners(products, 'products');
-buttonListeners(info, 'about');
-buttonListeners(aboutUs, 'advantages');
-buttonListeners(contacts, 'contacts');
+
+function scrollTo() {
+  smoother.scrollTo(`#home`, true);
+}
+
+home.addEventListener('click', scrollTo);
+
+// buttonListeners('products');
+// buttonListeners('about');
+// buttonListeners('advantages');
+// buttonListeners('contacts');
+// buttonListeners('home');
+
+// -------Version 2
+
+// function smoothScroll(target, duration) {
+//   var target = document.querySelector(target);
+//   var targetPosition = target.getBoundingClientRect().top;
+//   var startPosition = window.pageYOffset;
+//   var distance = targetPosition - startPosition;
+//   var startTime = null;
+
+//   function animation(currentTime) {
+//     if (startTime === null) startTime = currentTime;
+//     var timeElapsed = currentTime - startTime;
+//     var run = ease(timeElapsed, startPosition, distance, duration);
+//     window.scrollTo(0, run);
+//     if (timeElapsed < duration) requestAnimationFrame(animation);
+//   }
+
+//   function ease(t, b, c, d) {
+//     t /= d / 2;
+//     if (t < 1) return (c / 2) * t * t + b;
+//     t--;
+//     return (-c / 2) * (t * (t - 2) - 1) + b;
+//   }
+
+//   requestAnimationFrame(animation);
+// }
+
+// // Smooth scroll for navigation menu
+// var navLinks = document.querySelectorAll('nav a');
+
+// navLinks.forEach(function (link) {
+//   link.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     var target = this.getAttribute('href');
+//     var duration = 1000;
+//     smoothScroll(target, duration);
+//   });
+// });
